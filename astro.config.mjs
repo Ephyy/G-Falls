8bfe4551dc96a38ef97e313bcc14e9a0695b7ab0
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-09-03 17:44:46 -0400

    feat: Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'hybrid'
+  integrations: [tailwind(), react(), auth()],
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
