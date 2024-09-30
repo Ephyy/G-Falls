1f32a7ec1799501d3b3da44701b94a7989acb68c
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-09-30 01:53:08 -0300

    refactor GCursos Layout

--- src/components/GCursos/Servicios.jsx
+++ src/components/GCursos/Servicios.jsx
@@ -4,62 +4,71 @@ const servicios = [
   {
     ref: "/g-cursos/actas",
     icon: "https://static.u-cursos.cl/images/servicios/actas_v8712.svg",
+    name: "actas",
     title: "Acta",
   },
   {
     ref: "/g-cursos/clases",
     icon: "https://static.u-cursos.cl/images/servicios/clases_v8712.svg",
+    name: "clases",
     title: "Clase Virtual",
   },
   {
     ref: "/g-cursos/encuestas",
     icon: "https://static.u-cursos.cl/images/servicios/encuestas_v8712.svg",
+    name: "encuestas",
     title: "Encuestas",
   },
   {
     ref: "/g-cursos/foro",
     icon: "https://static.u-cursos.cl/images/servicios/foro_v8712.svg",
+    name: "foro",
     title: "Foro",
   },
   {
     ref: "/g-cursos/historial",
     icon: "https://static.u-cursos.cl/images/servicios/historial_v8712.svg",
+    name: "historial",
     title: "Historial",
   },
   {
     ref: "/g-cursos/notas",
     icon: "https://static.u-cursos.cl/images/servicios/notas_v8712.svg",
+    name: "notas",
     title: "Notas",
   },
   {
     ref: "/g-cursos/novedades",
     icon: "https://static.u-cursos.cl/images/servicios/novedades_v8712.svg",
+    name: "novedades",
     title: "Novedades",
   },
   {
     ref: "/g-cursos/tareas",
     icon: "https://static.u-cursos.cl/images/servicios/tareas_v8712.svg",
+    name: "tareas",
     title: "Tareas",
   },
   {
     ref: "/g-cursos/votaciones",
     icon: "https://static.u-cursos.cl/images/servicios/votaciones_v8712.svg",
+    name: "votaciones",
     title: "Votaciones",
   },
 ];
 
-const Servicios = ({ currentPath }) => {
-    // Encuentra el índice del servicio basado en la ruta actual
-    const initialSelectedIndex = servicios.findIndex(servicio => servicio.ref === currentPath);
+const Servicios = ({ nombreServicio }) => {
+    // Encuentra el índice del servicio basado en el titulo del servicio
+    const initialSelectedIndex = servicios.findIndex(servicio => servicio.name === nombreServicio);
   
     // Inicializa el estado con el índice del servicio basado en la ruta actual
     const [selectedService, setSelectedService] = useState(initialSelectedIndex);
   
     useEffect(() => {
       // Actualiza el estado cuando la ruta cambie
-      const currentIndex = servicios.findIndex(servicio => servicio.ref === currentPath);
+      const currentIndex = servicios.findIndex(servicio => servicio.name === nombreServicio);
       setSelectedService(currentIndex);
-    }, [currentPath]);
+    }, [nombreServicio]);
   
     const handleClick = (index) => {
       setSelectedService(index);
