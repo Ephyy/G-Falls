9ee3095b56017d82e1e175a64a001dc3b9dec214
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-04 19:28:06 -0300

    Add notas data file

---
+++ src/data/notas.ts
@@ -0,0 +1,134 @@
+export const notas = [
+    {
+      title: "Controles",
+      notas: [
+        {
+          id: 1,
+          nombre: "Control 1",
+          fecha: "9 de Septiembre",
+          publicada: true,
+        },
+      ],
+    },
+    {
+      title: "Ejercicios",
+      notas: [
+        {
+          id: 2,
+          nombre: "Ejercicio 1",
+          fecha: "9 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 3,
+          nombre: "Ejercicio 2",
+          fecha: "12 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 4,
+          nombre: "Ejercicio 3",
+          fecha: "16 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 5,
+          nombre: "Ejercicio 4",
+          fecha: "23 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 6,
+          nombre: "Ejercicio 5",
+          fecha: "26 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 7,
+          nombre: "Ejercicio 6",
+          fecha: "30 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 8,
+          nombre: "Ejercicio 8",
+          fecha: "23 de Septiembre",
+          publicada: true,
+        },
+      ],
+    },
+    {
+      title: "Ejercicios Laboratorios",
+      notas: [
+        {
+          id: 9,
+          nombre: "Laboratorio 01",
+          fecha: "13 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 10,
+          nombre: "Laboratorio 02",
+          fecha: "20 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 11,
+          nombre: "Laboratorio 3",
+          fecha: "27 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 12,
+          nombre: "Laboratorio 05",
+          fecha: "11 de Septiembre",
+          publicada: true,
+        },
+      ],
+    },
+    {
+      title: "Evaluaciones para el Acta de Notas",
+      notas: [
+        {
+          id: 13,
+          nombre: "Nota Final",
+          fecha: "",
+          publicada: false,
+        },
+      ],
+    },
+    {
+      title: "Laboratorios No Evaluados",
+      notas: [
+        {
+          id: 14,
+          nombre: "Laboratorio 04",
+          fecha: "3 de Septiembre",
+          publicada: true,
+        },
+      ],
+    },
+    {
+      title: "Tareas",
+      notas: [
+        {
+          id: 15,
+          nombre: "Tarea 1",
+          fecha: "21 de Agosto",
+          publicada: true,
+        },
+        {
+          id: 16,
+          nombre: "Tarea 2",
+          fecha: "23 de Septiembre",
+          publicada: false,
+        },
+        {
+          id: 17,
+          nombre: "Tarea 3",
+          fecha: "18 de Octubre",
+          publicada: false,
+        },
+      ],
+    },
+];
