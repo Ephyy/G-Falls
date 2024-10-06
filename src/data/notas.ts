dd954c2839c921f21a1f6ecabcc26c67b24a3a6c
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-06 02:20:10 -0300

    Refactor notas.ts to update property names and date formats

--- src/data/notas.ts
+++ src/data/notas.ts
@@ -1,132 +1,131 @@
 export const notas = [
     {
-      title: "Controles",
+      titulo: "Controles",
       notas: [
         {
           id: 1,
           nombre: "Control 1",
-          fecha: "9 de Septiembre",
+          fecha: 1725894000,
           publicada: true,
         },
       ],
     },
     {
-      title: "Ejercicios",
+      titulo: "Ejercicios",
       notas: [
         {
           id: 2,
           nombre: "Ejercicio 1",
-          fecha: "9 de Agosto",
+          fecha: 1723219200,
           publicada: true,
         },
         {
           id: 3,
           nombre: "Ejercicio 2",
-          fecha: "12 de Agosto",
+          fecha: 1723478400,
           publicada: true,
         },
         {
           id: 4,
           nombre: "Ejercicio 3",
-          fecha: "16 de Agosto",
+          fecha: 1723824000,
           publicada: true,
         },
         {
           id: 5,
           nombre: "Ejercicio 4",
-          fecha: "23 de Agosto",
+          fecha: 1724428800,
           publicada: true,
         },
         {
           id: 6,
           nombre: "Ejercicio 5",
-          fecha: "26 de Agosto",
+          fecha: 1724688000,
           publicada: true,
         },
         {
           id: 7,
           nombre: "Ejercicio 6",
-          fecha: "30 de Agosto",
+          fecha: 1725033600,
           publicada: true,
         },
         {
           id: 8,
           nombre: "Ejercicio 8",
-          fecha: "23 de Septiembre",
+          fecha: 1727103600,
           publicada: true,
         },
       ],
     },
     {
-      title: "Ejercicios Laboratorios",
+      titulo: "Ejercicios Laboratorios",
       notas: [
         {
           id: 9,
           nombre: "Laboratorio 01",
-          fecha: "13 de Agosto",
+          fecha: 1723564800,
           publicada: true,
         },
         {
           id: 10,
           nombre: "Laboratorio 02",
-          fecha: "20 de Agosto",
+          fecha: 1724169600,
           publicada: true,
         },
         {
           id: 11,
           nombre: "Laboratorio 3",
-          fecha: "27 de Agosto",
+          fecha: 1724774400,
           publicada: true,
         },
         {
           id: 12,
           nombre: "Laboratorio 05",
-          fecha: "11 de Septiembre",
+          fecha: 1726066800,
           publicada: true,
         },
       ],
     },
     {
-      title: "Evaluaciones para el Acta de Notas",
+      titulo: "Evaluaciones para el Acta de Notas",
       notas: [
         {
           id: 13,
           nombre: "Nota Final",
-          fecha: "",
           publicada: false,
         },
       ],
     },
     {
-      title: "Laboratorios No Evaluados",
+      titulo: "Laboratorios No Evaluados",
       notas: [
         {
           id: 14,
           nombre: "Laboratorio 04",
-          fecha: "3 de Septiembre",
+          fecha: 1725379200,
           publicada: true,
         },
       ],
     },
     {
-      title: "Tareas",
+      titulo: "Tareas",
       notas: [
         {
           id: 15,
           nombre: "Tarea 1",
-          fecha: "21 de Agosto",
+          fecha: 1724256000,
           publicada: true,
         },
         {
           id: 16,
           nombre: "Tarea 2",
-          fecha: "23 de Septiembre",
+          fecha: 1727103600,
           publicada: false,
         },
         {
           id: 17,
           nombre: "Tarea 3",
-          fecha: "18 de Octubre",
+          fecha: 1729263600,
           publicada: false,
         },
       ],
