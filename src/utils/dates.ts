005c9dbc601dd66a754bdc97adab8507ebf1adde
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-06 18:03:42 -0300

    Refactor formatDateTime function in dates.ts to support optional hour display

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,28 +1,43 @@
 
 
-export function formatDateTime(unixTimestamp: number) {
-    const date = new Date(unixTimestamp * 1000);
-  
-    // Formato móvil: 1 Oct
-    const optionsMobile: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short' };
-    const mobileFormat = date.toLocaleDateString('es-ES', optionsMobile);
-  
-    // Formato no móvil: 24 de Septiembre a las 12:30 hrs o 25 de Septiembre a las 23:59 hrs.
-    const optionsNoMobile: Intl.DateTimeFormatOptions = { 
-        day: 'numeric', 
-        month: 'long', 
-        hour: '2-digit', 
-        minute: '2-digit' 
+export function formatDateTime(unixTimestamp: number, withHour: boolean = false) {
+  const date = new Date(unixTimestamp * 1000);
+
+  // Formato móvil: 1 Oct
+  const optionsMobile: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short' };
+  const mobileFormat = date.toLocaleDateString('es-ES', optionsMobile);
+
+  let noMobileFormat = '';
+  let excelFormat = '';
+
+  if (withHour) {
+    const optionsNoMobileWithHour: Intl.DateTimeFormatOptions = { 
+      day: 'numeric', 
+      month: 'long', 
+      hour: '2-digit',
+      minute: '2-digit'
     };
-    let noMobileFormat = date.toLocaleDateString('es-ES', optionsNoMobile).replace(',', ' a las');
-    noMobileFormat += ' hrs.';
-  
+    noMobileFormat = date.toLocaleDateString('es-ES', optionsNoMobileWithHour)
+
     // Formato Excel: 2024-10-01 12:30:01
-    const excelFormat = date.toISOString().replace('T', ' ').split('.')[0];
-  
-    return {
-      mobileFormat,
-      noMobileFormat,
-      excelFormat
+    excelFormat = date.toISOString().replace('T', ' ').split('.')[0];
+  } else {
+    const optionsNoMobile: Intl.DateTimeFormatOptions = { 
+      day: 'numeric', 
+      month: 'long', 
     };
-}
+    noMobileFormat = date.toLocaleDateString('es-ES', optionsNoMobile)
+    // Formato Excel: 2024-10-01
+    excelFormat = date.toISOString().split('T')[0];
+  }
+
+
+  
+
+
+  return {
+    mobileFormat,
+    noMobileFormat,
+    excelFormat
+  }
+};
