28a4c195cc2e5c6b4e768f241f3124bdf62eb9ff
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-08 21:15:22 -0300

    add base getUser action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,21 @@
+import { defineAction } from 'astro:actions';
 import { user } from './user';
-
+import { pool } from '@/db/db.ts';
 
 export const server = {
-    user
+    getUsers: defineAction({
+        handler: async () => {
+            
+            const client = await pool.connect();
+            const res = await client.query(
+                `SELECT * FROM users`
+            );
+            client.release();
+
+            return res.rows;
+
+        }
+    }),
+    user,
 }
 
