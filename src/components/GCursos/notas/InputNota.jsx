5fa4f2bc7158de63c66646b257f7b67f3129d65e
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-11 13:13:56 -0300

    fix inputNota component

--- src/components/GCursos/notas/InputNota.jsx
+++ src/components/GCursos/notas/InputNota.jsx
@@ -1,41 +1,43 @@
 
 import { useState } from "react";
 
-function validarNota(nota) {
-  let numero = parseFloat(nota);
-
-  if (isNaN(numero)) {
-    return false;
-  }
-  // Parse:
-  numero = parseNota(nota);
-
-  if (numero < 1 || numero > 7) {
-    return false;
-  }
-
-  return true;
-}
 
 function parseNota(nota) {
+  // Quitamos cualquier caracter que no sea un número
+  nota = nota.replace(/[^0-9]/g, '');
+
   const entero = nota[0];
   const decimal = nota[1] ? nota[1].charAt(0) : '0';
-  return parseFloat(`${entero}.${decimal}`);
+
+  const parsed = `${entero}.${decimal}`
+  const parsedFloat = parseFloat(parsed);
+  
+
+  if (isNaN(parsedFloat) || parsedFloat < 1 || parsedFloat > 7) {
+    return "";
+  }
+  
+  return parsed;
 }
+
 
 function InputNota({value=''}) {
   const [inputValue, setInputValue] = useState(value);
   const [inputClass, setInputClass] = useState('');
 
   const handleChange = (event) => {
-    const newValue = event.target.value;
-    setInputValue(newValue);
+    const nota = event.target.value;
+    setInputValue(nota);
   }
 
   const handleBlur = (event) => {
-    const newValue = event.target.value;
+    const nota = event.target.value;
 
-    if (newValue !== value) {
+    // Parseamos la nota
+    const parsedNota = parseNota(nota);
+    setInputValue(parsedNota);
+    
+    if (parsedNota !== value) {
       setInputClass('rojo');
     } else {
       setInputClass('');
