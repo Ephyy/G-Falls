bbb052db773d9d14dfab1986c9cf2af454c1682d
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-11 18:49:25 -0300

    Refactor nota input component and add validation for nota value

--- src/actions/nota.ts
+++ src/actions/nota.ts
@@ -1,14 +1,23 @@
+import { defineAction } from 'astro:actions';
+import { z } from 'astro:schema';
 
-// import { defineAction } from 'astro:actions';
-// import { z } from 'astro:schema';
+const notaSchema = z.string().refine((val) => {
+  const parsed = parseFloat(val);
+  console.log(parsed);
+  return !isNaN(parsed) && parsed >= 1 && parsed <= 7;
+}, {
+  message: "El valor debe ser un número entre 1 y 7",
+});
 
-// export const nota = {
-//     cambiarNota: defineAction({
-//         accept: 'form',
-//         input: z.object({}),
-//         handler: async ({ input }) => {
-//             const { id, nota } = req.body;
-//             return { id, nota };
-//         }
-//     }),|
-// }
+export const nota = {
+  cambiarNota: defineAction({
+    accept: "form",
+    input: z.object({
+      valorNota: notaSchema,
+    }),
+    handler: async ({valorNota}) => {
+      console.log(`Nota cambiada a ${valorNota}`);
+      return 
+    }
+  })
+}
