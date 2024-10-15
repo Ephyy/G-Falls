90eb326c33c7b8f8537af7d019b2932f65aba8ef
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-15 20:05:54 -0300

    base mensajes

--- src/components/GCursos/Mensajes.jsx
+++ src/components/GCursos/Mensajes.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 
-function Mensajes({children}) {
+function Mensajes({mensajes}) {
+  useEffect(() => {
+    if (mensajes.length > 0) {
+      console.log('Mensajes actualizados:', mensajes);
+      // Aquí puedes agregar cualquier lógica adicional que necesites cuando los mensajes cambien
+      
+    }
+  }, [mensajes]);
+
   return (
-    <div id="mensajes" role="status">
-      {children}
+    <div id="mexito">
+    <a className="close">x</a>
+    <ul>
+      {mensajes?.map((message) => (
+        <li>{message}</li>
+      ))}
+    </ul>
     </div>
   )
 }
