10d78d6895cb5ee010fe6d84bed93acb398ca177
Ephy <vicente.ardiles@ug.uchile.cl>
2024-10-19 00:44:45 -0300

    Refactor CommandLine component to improve user input display

--- src/components/terminal/CommandLine.jsx
+++ src/components/terminal/CommandLine.jsx
@@ -1,13 +1,17 @@
 
 function CommandLine( {message, userInput} ) {
   return (
-    <div>
+    <div className="command-line">
         <div className="flex">
-            <span className="text-green-500">v@localhost</span>
-            <span>:</span>
-            <span className="text-fuchsia-600">/root</span>
-            <span>#</span>
-            <span>{userInput}</span>
+            <span className='user-info-container'>
+                <span className="text-green-500">v@localhost</span>
+                <span>:</span>
+                <span className="text-fuchsia-600">/root</span>
+            </span>
+            <span className='user-input-container'>
+              <span className='px-2'>#</span>
+              <span>{userInput}</span>
+            </span>
         </div>
         {message && (
             <div>
