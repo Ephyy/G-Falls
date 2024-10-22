b47d3237895957abcb1cf82b42bdf9f53a01aa85
Ephyy <vicente.ardiles@ug.uchile.cl>
2024-10-22 16:00:53 -0300

    update tareas layout

--- src/data/tareas.ts
+++ src/data/tareas.ts
@@ -2,10 +2,52 @@
 
 export const tareas = [
     {
-      titulo: "Laboratorio - Ejercicios Evaluados",
+      titulo: "Tareas",
       tarea: [
         {
           id: 1,
+          nombre: "Tarea 3 (parte A) - Los Mensajes Misteriosos del Misterio",
+          fechaInicial: 1729684800,
+          fechaFinal: 1731553199,
+          estado: "En Plazo",
+        },
+        {
+          id: 2,
+          nombre: "Tarea 2 - El Memorice Misterioso del Misterio",
+          fechaInicial: 1727092800,
+          fechaFinal: 1729133999,
+          estado: "Finalizada",
+        },
+        {
+          id: 3,
+          nombre: "Tarea 1 - La Máquina Tragamonedas del Misterio",
+          fechaInicial: 1724252400,
+          fechaFinal: 1726023599,
+          estado: "Finalizada",
+        },
+      ],
+    },
+    {
+      titulo: "Laboratorio - Ejercicios Evaluados",
+      tarea: [
+        {
+          id: 4,
+          nombre:
+            "Laboratorio 09 - Bodoque y la influencia de los Microplásticos🧃🧬",
+          fechaInicial: 1729611000,
+          fechaFinal: 1729738799,
+          estado: "Finalizada",
+        },
+        {
+          id: 5,
+          nombre:
+            "Laboratorio 08 - Bodoque está dando jugo 🥤🍎",
+          fechaInicial: 1728401400,
+          fechaFinal: 1728529199,
+          estado: "Finalizada",
+        },
+        {
+          id: 6,
           nombre:
             "Laboratorio 07 - A Bodoque se le desordenaron los tazos 🔴🔀ӽ ",
           fechaInicial: 1727796600,
@@ -13,7 +55,7 @@ export const tareas = [
           estado: "Finalizada",
         },
         {
-          id: 2,
+          id: 7,
           nombre:
             "Laboratorio 06 - Bodoque no quiere (puede) ir a comprar al Supermercado 🌷",
           fechaInicial: 1727191800,
@@ -21,7 +63,7 @@ export const tareas = [
           estado: "Finalizada",
         },
         {
-          id: 3,
+          id: 8,
           nombre:
             "Laboratorio 05 - Bodoque y el dilema de la Torta de Cumpleaños 🎂",
           fechaInicial: 1725982200,
@@ -29,7 +71,7 @@ export const tareas = [
           estado: "Finalizada",
         },
         {
-          id: 4,
+          id: 9,
           nombre:
             "[No Evaluado] Laboratorio 04 - Bodoque y el enigma de los diminutivos",
           fechaInicial: 1725379200,
@@ -37,21 +79,21 @@ export const tareas = [
           estado: "Finalizada",
         },
         {
-          id: 5,
+          id: 10,
           nombre: "Laboratorio 03 - Bodoque conoce al c.d. Anakena 🏆🏆👕",
           fechaInicial: 1724776200,
           fechaFinal: 1724903999,
           estado: "Finalizada",
         },
         {
-          id: 6,
+          id: 11,
           nombre: "Laboratorio 02 - Bodoque no ha entrenado para el TACO",
           fechaInicial: 1724171400,
           fechaFinal: 1724299199,
           estado: "Finalizada",
         },
         {
-          id: 7,
+          id: 12,
           nombre: "Laboratorio 01 - Bodoque no sabe leer fechas",
           fechaInicial: 1723564800,
           fechaFinal: 1723694399,
@@ -63,21 +105,28 @@ export const tareas = [
       titulo: "Ejercicios Cátedra",
       tarea: [
         {
-          id: 8,
+          id: 13,
+          nombre: "Ejercicio 11: Listas de Python entonces",
+          fechaInicial: 1729270800,
+          fechaFinal: 1729652399,
+          estado: "Finalizada",
+        },
+        {
+          id: 14,
           nombre: "Ejercicio 08 - Listas para volver del 18",
           fechaInicial: 1727114400,
           fechaFinal: 1727405999,
           estado: "Finalizada",
         },
         {
-          id: 9,
+          id: 15,
           nombre: "Ejercicio 07 - La aparición de las Faeranis",
           fechaInicial: 1726241400,
           fechaFinal: 1727233199,
           estado: "Finalizada",
         },
         {
-          id: 10,
+          id: 16,
           nombre: "Ejercicio 06 - Dividi Maxidi Ciu",
           fechaInicial: 1725037200,
           fechaFinal: 1725422399,
@@ -85,36 +134,10 @@ export const tareas = [
           estado: "Finalizada",
         },
         {
-          id: 11,
+          id: 17,
           nombre: "Ejercicio 01 - Hora de programar!",
           fechaInicial: 1723222800,
           fechaFinal: 1723607999,
-          estado: "Finalizada",
-        },
-      ],
-    },
-    {
-      titulo: "Tareas",
-      tarea: [
-        {
-          id: 12,
-          nombre: "Tarea 3",
-          fechaInicial: 1729233600,
-          fechaFinal: 1731313199,
-          estado: "En Plazo",
-        },
-        {
-          id: 13,
-          nombre: "Tarea 2 - El Memorice Misterioso del Misterio",
-          fechaInicial: 1727092800,
-          fechaFinal: 1729133999,
-          estado: "Finalizada",
-        },
-        {
-          id: 14,
-          nombre: "Tarea 1 - La Máquina Tragamonedas del Misterio",
-          fechaInicial: 1724252400,
-          fechaFinal: 1726023599,
           estado: "Finalizada",
         },
       ],
